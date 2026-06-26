5e561ac8b934f760b749955df661bf7eb00faa61
Vahit Keskin <36104238+vahitkeskin@users.noreply.github.com>
2026-06-26 20:34:33 +0300

    style: generate and integrate mathematical app icon for Android, iOS, and Desktop

--- desktopApp/src/main/kotlin/com/vahitkeskin/matatik/main.kt
+++ desktopApp/src/main/kotlin/com/vahitkeskin/matatik/main.kt
@@ -1,12 +1,15 @@
 package com.vahitkeskin.matatik
 
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 fun main() = application {
+    val icon = painterResource("app_icon.png")
     Window(
         onCloseRequest = ::exitApplication,
         title = "Matatik",
+        icon = icon
     ) {
         App()
     }
